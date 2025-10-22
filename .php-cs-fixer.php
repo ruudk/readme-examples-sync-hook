a7a73379aa6608534362cb96879e6430b4ddecdd
Ruud Kamphuis <ruudk@users.noreply.github.com>
2025-10-22 19:17:53 +0200

    Remove dependency on CaptainHook

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -8,6 +8,9 @@ use Ticketswap\PhpCsFixerConfig\RuleSet\TicketSwapRuleSet;
 
 $finder = Finder::create()
     ->in(__DIR__ . '/src')
-    ->append([__DIR__ . '/.php-cs-fixer.php']);
+    ->append([
+        __DIR__ . '/bin/readme-examples-sync',
+        __DIR__ . '/.php-cs-fixer.php',
+    ]);
 
 return PhpCsFixerConfigFactory::create(TicketSwapRuleSet::create())->setFinder($finder);
